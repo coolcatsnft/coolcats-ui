39585341fe114bb0c51f632c73217c421a2d04fa
= <alex@coolcatsnft.com>
2024-01-03 12:41:50 +0000

    chore: remove non avatar ui components

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,9 @@ export * from './constants';
 export * from './canvasUtils';
 export * from './Avatar/rules';
 export * from './Avatar/utils';
-export * from './partials';
-export * from './functions';
 export * from './GlobalStyle';
 
-export * from './Button';
-export * from './Icon';
-export * from './Dots';
-export * from './Label';
 export * from './LayeredCanvas';
 export * from './AvatarCanvas';
-export * from './Header';
-export * from './IconButton';
 
 export * as AvatarTypes from './Avatar/types';
